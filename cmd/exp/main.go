157b816ab7ebc8f8a3a1536f64882a137175b6bc
hayatu bayero <hbawazy@gmail.com>
2022-01-26 16:52:32 +0100

    cross scripting

---
+++ cmd/exp/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Bio  string
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+
+	if err != nil {
+		panic(err)
+	}
+	user := User{
+		Name: "Hayatu Smith",
+		Bio:  `<script> alert("HAha you have been hacked")</script>`,
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
