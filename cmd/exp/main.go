ce1be7f557029267d2534f3efcf9c1b245faa377
hayatu bayero <hbawazy@gmail.com>
2022-09-24 18:07:54 +0100

    worked on making multiple queries with go

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -74,25 +74,57 @@ func main() {
 	// 	panic(err)
 	// }
 	// fmt.Println("User created! id= ", id)
-	id := 1
-	row := db.QueryRow(`
-		SELECT name,email FROM users WHERE id=$1;`, id)
-	var name, email string
-	err = row.Scan(&name, &email)
+	// id := 1
+	// row := db.QueryRow(`
+	// 	SELECT name,email FROM users WHERE id=$1;`, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("User information: name=%s, email=%s\n", name, email)
+	// userId := 1
+	// for i := 1; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake Order #%d", i)
+	// 	_, err := db.Exec(`
+	// INSERT INTO orders(user_id,amount,description)
+	// VALUES($1,$2,$3);`, userId, amount, desc)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("Added fake orders")
+
+	type Order struct {
+		Id          int
+		User_id     int
+		Amount      string
+		description string
+	}
+	var orders []Order
+	userId := 1
+	rows, err := db.Query(`
+	SELECT id,amount,description
+	FROM orders
+	WHERE user_id=$1`, userId)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("User information: name=%s, email=%s\n", name, email)
-	userId := 1
-	for i := 1; i <= 5; i++ {
-		amount := i * 100
-		desc := fmt.Sprintf("Fake Order #%d", i)
-		_, err := db.Exec(`
-	INSERT INTO orders(user_id,amount,description)
-	VALUES($1,$2,$3);`, userId, amount, desc)
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.User_id = userId
+		err := rows.Scan(&order.Id, &order.Amount, &order.description)
 		if err != nil {
 			panic(err)
 		}
+		orders = append(orders, order)
 	}
-	fmt.Println("Added fake orders")
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Orders:", orders)
 }
